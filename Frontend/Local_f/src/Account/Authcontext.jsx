edb956b7ffd132f0b24bd1bea38a963e9c58e71d
adarsh <adarshaq75@gmail.com>
2026-04-23 18:32:50 +0530

    pro

--- Frontend/Local_f/src/Account/Authcontext.jsx
+++ Frontend/Local_f/src/Account/Authcontext.jsx
@@ -1,24 +1,36 @@
 import { createContext, useEffect, useState } from "react";
 import api from "../api/axios";
-import axios from "axios";
+
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
-  
-  useEffect(()=>{
-      api.get("Navbar/", {
-      withCredentials: true
-    })
-    .then((res) => {
-      setUser(res.data);
-    })
-    .catch((err) => console.log(err));
-  },[])
+  const [loading, setLoading] = useState(true);
 
+  useEffect(() => {
+    const token = localStorage.getItem("access_token");
+
+    if (!token) {
+      setLoading(false);
+      return;
+    }
+
+    api.get("Navbar/", {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((res) => {
+        setUser(res.data);
+      })
+      .catch(() => {
+        setUser(null);
+      })
+      .finally(() => setLoading(false));
+  }, []);
 
   return (
-    <AuthContext.Provider value={{ user, setUser }}>
+    <AuthContext.Provider value={{ user, setUser, loading }}>
       {children}
     </AuthContext.Provider>
   );
