6b3beadeafd6c286432d4f0f1abed384a80c0f27
adarsh <adarshaq75@gmail.com>
2026-04-23 17:49:02 +0530

    midlware

--- Backend/local_service/local_service/settings.py
+++ Backend/local_service/local_service/settings.py
@@ -82,6 +82,7 @@ SITE_ID = 1
 
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -89,7 +90,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'allauth.account.middleware.AccountMiddleware',
 
     
