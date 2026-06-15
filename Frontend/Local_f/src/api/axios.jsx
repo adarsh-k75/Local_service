0d3d6cf007dcb17134eb312d3e55495b30131944
adarsh <adarshaq75@gmail.com>
2026-06-15 21:09:18 +0530

    api

--- Frontend/Local_f/src/api/axios.jsx
+++ Frontend/Local_f/src/api/axios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/",
+  baseURL: "http://localhost:8000/api/",
   withCredentials: true,
 });
 
@@ -18,7 +18,7 @@ api.interceptors.response.use(
         
        
         await axios.post(
-          "http://127.0.0.1:8000/api/refresh/", 
+          "http://localhost:8000/api/refresh/", 
           {}, 
           { withCredentials: true }
         );
