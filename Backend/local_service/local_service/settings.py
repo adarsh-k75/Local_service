279d797232de045820ec3fa55ae7b61226818c28
adarsh <adarshaq75@gmail.com>
2026-06-15 21:41:44 +0530

    updated backend settings

--- Backend/local_service/local_service/settings.py
+++ Backend/local_service/local_service/settings.py
@@ -42,7 +42,7 @@ SECRET_KEY = 'django-insecure-+lb+!#%v!y-6sipvtlxha9^7!fzhzdqg+%a37sjblsfctmthb_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['13.48.70.152', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -128,7 +128,7 @@ DATABASES = {
         'HOST': os.getenv("DB_HOST"),
         'PORT': os.getenv("DB_PORT", "5432"),
           'OPTIONS': {
-            'sslmode': 'disable',  # VERY IMPORTANT for Render Postgres
+            'sslmode': 'require',  # VERY IMPORTANT for Render Postgres
         }
     }
 }
